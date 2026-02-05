08a2e01c7331fdd285bf705d9a05e1f54db419f9
O001830 <fernando.aguilar.santos@bbva.com>
2026-02-05 17:32:10 +0100

    Fix firebaseConfig module for browser

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -1,21 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Configuración de Firebase (solo datos, sin imports)
+export const firebaseConfig = {
   apiKey: "AIzaSyCRV9Vh0QSA0RQ50r8SZwt72PIwjhPoL5o",
   authDomain: "owl-manage.firebaseapp.com",
   projectId: "owl-manage",
   storageBucket: "owl-manage.firebasestorage.app",
   messagingSenderId: "971812158",
   appId: "1:971812158:web:42a468ad62bd204365106f",
-  measurementId: "G-N3SCJV1H6Z"
+  measurementId: "G-N3SCJV1H6Z",
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
